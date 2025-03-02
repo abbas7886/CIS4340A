8f223e6c8d859c3c3720d5db65c040cd280b05a4
abbas7886 <125617851+abbas7886@users.noreply.github.com>
2025-03-02 17:06:17 -0500

    Update R03_NUM04_J.java
    CODE EDITED TO FIX SECURITY VULNERABILITIES

--- R03_NUM04_J.java
+++ R03_NUM04_J.java
@@ -1,6 +1,6 @@
-double dollar = 1.00;
-double dime = 0.10;
+int dollar = 100;
+int dime = 10;
 int number = 7;
 System.out.println(
-  "A dollar less " + number + " dimes is $" + (dollar - number * dime) 
+  "A dollar less " + number + " dimes is $0." + (dollar - number * dime)
 );
